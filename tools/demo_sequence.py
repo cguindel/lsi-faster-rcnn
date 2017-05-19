edbc47d727d512e394bfab8b8c9cefdf222dbae7
Carlos Guindel <cguindel@ing.uc3m.es>
2017-05-19 18:34:52 +0200

    Add updated demos

--- tools/demo_sequence.py
+++ tools/demo_sequence.py
@@ -19,26 +19,31 @@ from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
 import glob
 
-CLASSES = ('__background__',
-           'aeroplane', 'bicycle', 'bird', 'boat',
-           'bottle', 'bus', 'car', 'cat', 'chair',
-           'cow', 'diningtable', 'dog', 'horse',
-           'motorbike', 'person', 'pottedplant',
-           'sheep', 'sofa', 'train', 'tvmonitor')
-
 NETS = {'vgg16': ('VGG16',
-                  'VGG16_faster_rcnn_final.caffemodel'),
+                  'VGG16_faster_rcnn_final.caffemodel',
+                  'faster_rcnn_models',
+                  'faster_rcnn_alt_opt',
+                  'pascal_voc',
+                  15), # Pedestrian class
         'zf': ('ZF',
-                'ZF_faster_rcnn_final.caffemodel'),
-        'caviar': ('caviar',
-                'caviar.caffemodel')}
+                'ZF_faster_rcnn_final.caffemodel',
+                'faster_rcnn_models',
+                'faster_rcnn_alt_opt',
+                'pascal_voc',
+                15),
+        'caviar': ('VGG16',
+                'lsi_caviar_vgg16.caffemodel',
+                'lsi_models',
+                'faster_rcnn_end2end',
+                'caviar',
+                1)}
+
+SEQUENCE = 'surveillance'
 
 # video recorder
 fourcc = cv2.VideoWriter_fourcc(*'H264')
@@ -76,7 +81,7 @@ def demo(net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
 
     # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'surveillance', image_name)
+    im_file = os.path.join(cfg.DATA_DIR, SEQUENCE, image_name)
     im = cv2.imread(im_file)
 
     # Detect all object classes and regress object bounds
@@ -90,10 +95,7 @@ def demo(net, image_name):
     # Visualize detections for each class
     CONF_THRESH = 0.01
     NMS_THRESH = 0.3
-    if args.demo_net == 'caviar':
-        cls_ind = 1
-    else:
-        cls_ind = 15
+    cls_ind = int(NETS[args.demo_net][5])
     cls = 'person'
     cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
     cls_scores = scores[:, cls_ind]
@@ -126,25 +128,26 @@ if __name__ == '__main__':
     cfg.VIEWPOINTS = False
     cfg.TEST.SCALES = [500]
     cfg.TEST.MAX_SIZE = 3000
-    #cfg.TEST.RPN_POST_NMS_TOP_N = 600
-    #cfg.TEST.EXTERNAL_ROIS = True
 
     args = parse_args()
 
+    cfg.MODELS_DATASET = NETS[args.demo_net][4]
+    cfg.MODELS_DIR = os.path.abspath(os.path.join(cfg.ROOT_DIR, 'models', \
+        cfg.MODELS_DATASET))
+
     prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
-                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
-                              NETS[args.demo_net][1])
+                            NETS[args.demo_net][3], 'faster_rcnn_test.pt')
+    caffemodel = os.path.join(cfg.DATA_DIR, NETS[args.demo_net][2],
+                            NETS[args.demo_net][1])
+
+    print 'Prototxt: ', prototxt
+    print 'Caffemodel: ', caffemodel
 
     if args.record>0:
         video_writer = cv2.VideoWriter("output.avi", fourcc, 25, (384, 288))
 
-    # prototxt = '/home/cguindel/lsi-faster-rcnn/models/kitti/VGG16/caviar/test.prototxt'
-    # caffemodel = '/home/cguindel/lsi-faster-rcnn/output/caviar/kitti_caviar_train/vgg16_faster_rcnn_iter_100000.caffemodel'
-
     if not os.path.isfile(caffemodel):
-        raise IOError(('{:s} not found.\nDid you run ./data/script/'
-                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+        raise IOError(('{:s} not found.').format(caffemodel))
 
     if args.cpu_mode:
         caffe.set_mode_cpu()
